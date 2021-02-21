e4c05242b57a73329111006cb71162817c7d5eee
elichka <ed44362072@edu.mon.bg>
2021-02-21 11:14:02 +0200

    Create project structure and add custom DbContext without implementation

---
+++ src/Gamebase/Gamebase.Data/GamebaseDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Gamebase.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class GamebaseDbContext : IdentityDbContext
+    {
+        public GamebaseDbContext(DbContextOptions<GamebaseDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
---
+++ src/Gamebase/Gamebase.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gamebase.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
